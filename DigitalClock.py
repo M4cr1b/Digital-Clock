7106702b182407f7024930ec9ff1d0fea72f78c6
M4cr1b <159058241+M4cr1b@users.noreply.github.com>
2024-06-28 23:02:47 +0000

    Update DigitalClock.py

--- DigitalClock.py
+++ DigitalClock.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import Label
 from time import strftime
 
+
 def time():
     time_string = strftime('%H:%M:%S %p')
     date_string = strftime('%A, %B %d, %Y')
@@ -14,7 +15,7 @@ root.title('Digital Clock')
 
 Label(root, font=("Courier New",30, "bold"), text="Digital Clock", fg="white", bg="grey").pack()
 
-label_time = tk.Label(root, font=('calibri', 40, 'bold'), background='grey', foreground='white')
+label_time = tk.Label(root, font=('Source Sans Pro Regular', 40, 'bold'), background='grey', foreground='white')
 label_time.pack(anchor='center')
 
 label_date = tk.Label(root, font=('calibri', 40, 'bold'), background='grey', foreground='white')
